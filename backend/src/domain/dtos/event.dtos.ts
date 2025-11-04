8f0c2f34aa599fa612a21890b0b0b1ff1b1a7070
chicongn <chicongvn1205@gmail.com>
2025-11-04 16:55:42 +0700

    feat(event): add event entity and props

---
+++ backend/src/domain/dtos/event.dtos.ts
@@ -0,0 +1,32 @@
+import { EventStatus, EventCategory } from "../../application/dtos/enums";
+
+export interface CreateEventProps {
+    id: string;
+    name: string;
+    location: string;
+    startTime: Date;
+    endTime: Date | null;
+    description: string;
+    imageUrl: string;
+    categories: EventCategory[];
+    capacity: number;
+    status: EventStatus;
+    ownerId: string;
+    eventManagerIds: string[];
+    participantIds: string[];
+    registerUserIds: string[];
+    registerCount?: number;
+    updatedAt?: Date;
+}
+
+export interface UpdateEventProps {
+    name: string;
+    location: string;
+    startTime: Date;
+    endTime: Date | null;
+    description: string;
+    imageUrl: string;
+    categories: EventCategory[];
+    capacity: number;
+    eventManagerIds: string[];
+}
