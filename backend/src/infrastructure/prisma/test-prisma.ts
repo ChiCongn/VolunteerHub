920f1ada522127d524a6a09eab47f691d946e74c
chicongn <chicongvn1205@gmail.com>
2025-11-07 14:51:37 +0700

    build(env): change type commonjs to module

--- backend/src/infrastructure/prisma/test-prisma.ts
+++ backend/src/infrastructure/prisma/test-prisma.ts
@@ -1,15 +1,19 @@
-import 'dotenv/config';
-import { PrismaClient } from './generated/client';
+import "dotenv/config";
+import { PrismaClient } from "./generated/client";
 
 const prisma = new PrismaClient();
 
 async function testPrimsma() {
     try {
-        const users = await prisma.users.findMany();
+        //const users = await prisma.users.findMany();
+        const users = await prisma.$queryRawUnsafe(
+            `SELECT * FROM users
+            WHERE id = '70cc9a01-9d4c-4d2a-a6a9-68b9888bf5ce'`
+        );
 
-        console.log('✅ Found users:', users);
+        console.log("✅ Found users:", users);
     } catch (error) {
-        console.error('❌ Error querying database:', error);
+        console.error("❌ Error querying database:", error);
     } finally {
         await prisma.$disconnect();
     }
