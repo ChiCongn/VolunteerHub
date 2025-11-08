421bfa43dbe2fae0e7cef9be95d3e855d4afe988
chicongn <chicongvn1205@gmail.com>
2025-11-08 23:25:14 +0700

    feat: add logger and vitest testing framework

---
+++ backend/src/logger.ts
@@ -0,0 +1,15 @@
+import pino from 'pino';
+
+const logger = pino({
+    level: process.env.PINO_LOG_LEVEL || 'info',
+    formatters: {
+        level: (label: string) => ({ level: label.toUpperCase() }),
+    },
+    timestamp: pino.stdTimeFunctions.isoTime,
+});
+
+if (process.env.NODE_ENV === 'test') {
+    logger.level = 'silent';
+}
+
+export default logger;
