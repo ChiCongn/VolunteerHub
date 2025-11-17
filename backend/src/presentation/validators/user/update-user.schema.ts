930db6a173a61591ff555a9488bf5718f48c9157
chicongn <chicongvn1205@gmail.com>
2025-11-17 11:32:06 +0700

    feat(zod): create zod schema for validating update user and list user filter request input

---
+++ backend/src/presentation/validators/user/update-user.schema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const UpdateUserSchema = z.object({
+    username: z.string().min(3, { message: "Username must be at least 3 characters" }).optional(),
+
+    password: z
+        .string()
+        .min(8, "Password must be at least 8 characters")
+        .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+        .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+        .regex(/[0-9]/, "Password must contain at least one number")
+        .regex(/[^A-Za-z0-9]/, "Password must contain at least one special character")
+        .optional(),
+
+    avatarUrl: z.string().url({ message: "Invalid URL format" }).optional(),
+});
+
+export type UpdateUserRequest = z.infer<typeof UpdateUserSchema>;
