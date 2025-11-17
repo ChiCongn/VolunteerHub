a44f1b5a87b7d9269a19fc00a5729cd312fc8888
chicongn <chicongvn1205@gmail.com>
2025-11-17 14:29:21 +0700

    feat(zod): create zod schema for validating notification create, filter request

---
+++ backend/src/presentation/validators/notification/create-notification.schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { NotificationType } from "../../../domain/entities/enums";
+import { UserIdSchema } from "../param/id.schema";
+
+export const CreateNotificationSchema = z.object({
+    userId: UserIdSchema,
+    type: z.nativeEnum(NotificationType),
+    message: z
+        .string()
+        .trim()
+        .min(1, "Message is required")
+        .max(500, "Message cannot exceed 500 characters"),
+    redirectUrl: z.union([z.string().url("Invalid redirect URL"), z.literal("")]).optional(),
+});
+
+export type CreateNotificationRequest = z.infer<typeof CreateNotificationSchema>;
---
+++ backend/src/presentation/validators/notification/filter-notification.schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+import { NotificationType } from "../../../domain/entities/enums";
+
+export const NotificationFilterSchema = z.object({
+    read: z
+        .enum(["true", "false"])
+        .transform((val) => val === "true")
+        .optional(),
+    type: z.nativeEnum(NotificationType).optional(),
+});
+
+export type NotificationFilter = z.infer<typeof NotificationFilterSchema>;
