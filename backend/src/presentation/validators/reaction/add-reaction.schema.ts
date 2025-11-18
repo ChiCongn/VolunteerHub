949068c346f3de126d25c07e2c0a7b8670ae1e9e
chicongn <chicongvn1205@gmail.com>
2025-11-18 09:24:45 +0700

    feat(zod schema): wrap schemas in template

--- backend/src/presentation/validators/reaction/add-reaction.schema.ts
+++ backend/src/presentation/validators/reaction/add-reaction.schema.ts
@@ -2,9 +2,13 @@ import { z } from "zod";
 import { EmojiType } from "../../../domain/entities/enums";
 import { PostIdSchema, UserIdSchema } from "../param/id.schema";
 
-export const AddReactionSchema = z.object({
-    postId: PostIdSchema,
-    emoji: z.nativeEnum(EmojiType, "reaction is required"),
-});
+export const AddReactionSchema = {
+    params: z.object({
+        postId: PostIdSchema,
+    }),
+    body: z.object({
+        emoji: z.nativeEnum(EmojiType, "reaction is required"),
+    }),
+};
 
 export type AddReactionRequest = z.infer<typeof AddReactionSchema>;
--- backend/src/presentation/validators/reaction/update-reaction.schema.ts
+++ backend/src/presentation/validators/reaction/update-reaction.schema.ts
@@ -1,8 +1,15 @@
 import { z } from "zod";
 import { EmojiType } from "../../../domain/entities/enums";
+import { PostIdSchema, ReactionIdSchema } from "../param/id.schema";
 
-export const UpdateReactionSchema = z.object({
-    emoji: z.nativeEnum(EmojiType, "reaction is required"),
-});
+export const UpdateReactionSchema = {
+    params: z.object({
+        postId: PostIdSchema,
+        reactionId: ReactionIdSchema,
+    }),
+    body: z.object({
+        emoji: z.nativeEnum(EmojiType, "reaction is required"),
+    }),
+};
 
 export type UpdateReactionRequest = z.infer<typeof UpdateReactionSchema>;
