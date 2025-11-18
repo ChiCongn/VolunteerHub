f145eb1f48016f50239532b27bcaf1802118eda4
chicongn <chicongvn1205@gmail.com>
2025-11-18 09:44:04 +0700

    feat(zod schema): wrap schemas in a template

--- backend/src/presentation/validators/notification/filter-notification.schema.ts
+++ backend/src/presentation/validators/notification/filter-notification.schema.ts
@@ -1,12 +1,14 @@
 import { z } from "zod";
 import { NotificationType } from "../../../domain/entities/enums";
 
-export const NotificationFilterSchema = z.object({
-    read: z
-        .enum(["true", "false"])
-        .transform((val) => val === "true")
-        .optional(),
-    type: z.nativeEnum(NotificationType).optional(),
-});
+export const NotificationFilterSchema = {
+    query: z.object({
+        read: z
+            .enum(["true", "false"])
+            .transform((val) => val === "true")
+            .optional(),
+        type: z.nativeEnum(NotificationType).optional(),
+    }),
+};
 
 export type NotificationFilter = z.infer<typeof NotificationFilterSchema>;
