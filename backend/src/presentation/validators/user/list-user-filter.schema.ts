bdf9128bc6b6681a5abdb77045bb62ec5d573131
chicongn <chicongvn1205@gmail.com>
2025-12-01 15:39:20 +0700

    feat(validator): add paging and sort in query zod schema

--- backend/src/presentation/validators/user/list-user-filter.schema.ts
+++ backend/src/presentation/validators/user/list-user-filter.schema.ts
@@ -6,6 +6,29 @@ export const ListUserFilterSchema = {
         role: z.nativeEnum(UserRole, { message: "Invalid role" }).optional(),
         status: z.nativeEnum(UserStatus, { message: "Invalid status" }).optional(),
         search: z.string().min(1, { message: "Search query must not be empty" }).optional(),
+        page: z
+            .string()
+            .regex(/^\d+$/, { message: "Page must be a number" })
+            .transform(Number)
+            .default(1) // maigc value, maybe replace by env
+            .optional(),
+        limit: z
+            .string()
+            .regex(/^\d+$/, { message: "Limit must be a number" })
+            .transform(Number)
+            .default(20) // magic value, maybe replace by env (impose hard limit 100 in backend)
+            .optional(),
+        sortBy: z
+            .enum(["username", "email", "createdAt"], {
+                message: "Invalid sorting field",
+            })
+            .default("createdAt")
+            .optional(),
+
+        order: z
+            .enum(["asc", "desc"], { message: "Invalid sorting order" })
+            .default("desc")
+            .optional(),
     }),
 };
 
