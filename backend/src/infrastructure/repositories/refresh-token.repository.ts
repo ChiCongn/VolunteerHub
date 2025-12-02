f6edc12a237984b19928cf1b4953836c52d30822
chicongn <chicongvn1205@gmail.com>
2025-12-02 08:52:59 +0700

    feat(repo): implement refresh token repo interface

--- backend/src/infrastructure/repositories/refresh-token.repository.ts
+++ backend/src/infrastructure/repositories/refresh-token.repository.ts
@@ -1,8 +1,11 @@
 import { CreateRefreshTokenDto } from "../../application/dtos/refresh-token/create-refresh-token.dto";
+import { RefreshTokenDto } from "../../application/dtos/refresh-token/refresh-token.dto";
+import { RefreshTokenNotFoundError } from "../../domain/errors/auth.error";
+import { IRefreshTokenRepository } from "../../domain/repositories/refresh-token.irepository";
 import logger from "../../logger";
 import { PrismaClient } from "../prisma/generated/client";
 
-export class RefreshTokenRepository {
+export class RefreshTokenRepository implements IRefreshTokenRepository {
     constructor(private readonly prisma: PrismaClient) {}
 
     async create(data: CreateRefreshTokenDto) {
@@ -20,19 +23,46 @@ export class RefreshTokenRepository {
         });
     }
 
-    async findByToken(token: string) {
+    async findByToken(token: string): Promise<RefreshTokenDto> {
         logger.debug(
             { token, action: "findByToken" },
             "[RefreshTokenRepository] Finding refresh token"
         );
-        return this.prisma.refresh_tokens.findUnique({ where: { token } });
+        const tokenRecord = await this.prisma.refresh_tokens.findUnique({ where: { token } });
+
+        if (!tokenRecord) {
+            logger.warn(
+                { token, action: "findByToken" },
+                "[RefreshTokenRepository] Refresh token not found"
+            );
+            throw new RefreshTokenNotFoundError();
+        }
+
+        return {
+            id: tokenRecord.id,
+            userId: tokenRecord.user_id,
+            token: tokenRecord.token,
+            expiresAt: tokenRecord.expires_at,
+            revoked: tokenRecord.revoked,
+        };
     }
 
-    async revoke(token: string) {
+    async deleteToken(token: string): Promise<void> {
+        logger.debug(
+            { token, action: "deleteToken" },
+            "[RefreshTokenRepository] Deleting refresh token"
+        );
+
+        await this.prisma.refresh_tokens.delete({
+            where: { token },
+        });
+    }
+
+    async revoke(token: string): Promise<void> {
         logger.debug(
             { token, action: "revoke" },
             "[RefreshTokenRepository] Revoking refresh token"
         );
-        return this.prisma.refresh_tokens.update({ where: { token }, data: { revoked: true } });
+        await this.prisma.refresh_tokens.update({ where: { token }, data: { revoked: true } });
     }
 }
