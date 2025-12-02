a5a25265a5b38aa1145129485505e72dcc7dd75a
chicongn <chicongvn1205@gmail.com>
2025-12-02 17:03:16 +0700

    chore: rename display name to username

--- backend/src/presentation/validators/user/search-user.schema.ts
+++ backend/src/presentation/validators/user/search-user.schema.ts
@@ -2,6 +2,6 @@ import { z } from "zod";
 
 export const SearchUserSchema = {
     query: z.object({
-        displayName: z.string().min(1, { message: "Display name must not be empty" }),
+        username: z.string().min(1, { message: "Display name must not be empty" }),
     }),
 };
