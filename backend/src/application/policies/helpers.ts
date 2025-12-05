a0b41592c1fe0e5f48ffd16a2ad705b263c91251
chicongn <chicongvn1205@gmail.com>
2025-12-05 14:23:04 +0700

    feat(authorize): add helper functions to help authorize

---
+++ backend/src/application/policies/helpers.ts
@@ -0,0 +1,190 @@
+import { Request } from "express";
+import { UserRole, UserStatus } from "../../domain/entities/enums";
+import { userService } from "../service/user.service";
+import { ForbiddenError } from "../../domain/errors/user.error";
+import logger from "../../logger";
+import { eventService } from "../service/event.service";
+import { postService } from "../service/post.service";
+import { commentService } from "../service/comment.service";
+import { notificationService } from "../service/notification.service";
+
+export interface AuthContext {
+    id: string;
+    role: UserRole;
+    status: UserStatus;
+}
+
+export const ResourceType = {} as const;
+export type ResourceType = (typeof ResourceType)[keyof typeof ResourceType];
+
+// 1. Build Auth Context
+
+// JWT contains only a snapshot,
+// user status or role might have changed since token was issued.
+// Reload from DB ensures real-time correctness for sensitive authorization checks
+export const buildAuthContext = async (req: Request): Promise<AuthContext> => {
+    const id = req.user.sub;
+    logger.debug(
+        {
+            action: "buildAuthContext",
+            method: req.method,
+            url: req.originalUrl || req.url,
+            userId: id,
+        },
+        `[Authorization] Building AuthContext for user ${id})`
+    );
+    return userService.getAuthContext(id);
+};
+
+// 2. Role & Status Checks
+export const requireRole = (role: UserRole, permittedRoles: UserRole[]) => {
+    logger.debug(
+        { action: "requireRole", currentRole: role, permittedRoles },
+        `[Authorization] Checking role ${role} against [${permittedRoles.join(", ")}]`
+    );
+
+    if (!permittedRoles.includes(role)) {
+        logger.error(
+            { attemptedRole: role, permittedRoles: permittedRoles, action: "requireRole" },
+            "[Authorization] Role is not permitted"
+        );
+        throw new ForbiddenError();
+    }
+};
+
+export const requireActiveStatus = (status: UserStatus) => {
+    logger.debug(
+        { action: "requireActiveStatus", userStatus: status },
+        `[Authorization] Checking user status: ${status}`
+    );
+
+    if (status !== UserStatus.Active) {
+        logger.error(
+            { userStatus: status, action: "requireActiveStatus" },
+            "[Authorization] User status is not allowed"
+        );
+        throw new ForbiddenError();
+    }
+};
+
+// 3. Event Permissions
+export const requireOwnerEvent = async (userId: string, eventId: string) => {
+    logger.debug(
+        { action: "requireOwnerEvent", userId, eventId },
+        `[Authorization] Checking event ownership for user ${userId}`
+    );
+
+    const ownerId = await eventService.findOwnerId(eventId);
+    if (userId !== ownerId) {
+        logger.error(
+            { userId, action: "requireOwnerEvent" },
+            `[Authorization] User with ID: ${userId} is not owner of event ${eventId}`
+        );
+        throw new ForbiddenError();
+    }
+};
+
+export const requireManagerEvent = async (userId: string, eventId: string) => {
+    logger.debug(
+        { action: "requireManagerEvent", userId, eventId },
+        `[Authorization] Checking event manager rights for user ${userId}`
+    );
+
+    const managerIds = await eventService.findManagerIds(eventId);
+
+    if (!managerIds.includes(userId)) {
+        logger.error(
+            { userId, action: "requireManagerEvent" },
+            `[Authorization] User with ID: ${userId} is not a manager of event ${eventId}`
+        );
+        throw new ForbiddenError();
+    }
+};
+
+// Checks if a user is allowed to view or interact with an event
+// Used for read operations and as a foundation for post/comment access
+export const canAccessEvent = (userId: string, eventId: string) => {};
+
+// 4. Post Permissions
+export const requireAuthorPost = async (userId: string, postId: string) => {
+    logger.debug(
+        { action: "requireAuthorPost", userId, postId },
+        `[Authorization] Checking post authorship for user ${userId}`
+    );
+
+    const authorId = await postService.findAuthorId(postId);
+    if (userId !== authorId) {
+        logger.error(
+            { userId, action: "requireAuthorPost" },
+            `[Authorization] User with ID: ${userId} is not author of post ${postId}`
+        );
+        throw new ForbiddenError();
+    }
+};
+
+// users can view a post only if they are enrolled in (or participants of) the event it belongs to
+export const canAccessPost = async (userId: string, postId: string) => {
+    logger.debug(
+        { action: "canAccessPost", userId, postId },
+        `[Authorization] Checking post access for user ${userId}`
+    );
+    const eventId = await postService.findEventIdByPostId(postId);
+
+    return eventService.isParticipant(userId, eventId);
+};
+
+// 5. Comment Permissions
+export const requireAuthorComment = async (userId: string, commentId: string) => {
+    logger.debug(
+        { action: "requireAuthorComment", userId, commentId },
+        `[Authorization] Checking comment authorship for user ${userId}`
+    );
+
+    const authorId = await commentService.findAuthorId(commentId);
+
+    if (userId !== authorId) {
+        logger.error(
+            { userId, action: "requireAuthorComment" },
+            `[Authorization] User with ID: ${userId} is not author of comment ${commentId}`
+        );
+        throw new ForbiddenError();
+    }
+};
+
+// users can view a comment only if they are enrolled in (or participants of) the event
+// that has the post it belongs to
+export const canAccessComment = async (userId: string, commentId: string) => {
+    logger.debug(
+        { action: "canAccessComment", userId, commentId },
+        `[Authorization] Checking comment access for user ${userId}`
+    );
+    const postId = await commentService.findPostIdByCommentId(commentId);
+
+    return canAccessPost(userId, postId);
+};
+
+// 6. Reaction Permissions
+export const requireAuthorReaction = (userId: string, reactionId: string) => {
+    logger.debug(
+        { action: "requireAuthorReaction", userId, reactionId },
+        `[Authorization] Checking reaction ownership for user ${userId}`
+    );
+};
+
+// 7. Notification Permissions
+export const requireOwnerNotification = async (userId: string, notificationId: string) => {
+    logger.debug(
+        { action: "requireOwnerNotification", userId, notiId: notificationId },
+        `[Authorization] Checking notification ownership for user ${userId}`
+    );
+
+    const ownerId = await notificationService.findOwnerId(notificationId);
+
+    if (userId !== ownerId) {
+        logger.error(
+            { action: "requireOwnerNotification", userId, notiId: notificationId, ownerId },
+            `[Authorization] User ${userId} does not own notification ${notificationId}`
+        );
+        throw new ForbiddenError();
+    }
+};
