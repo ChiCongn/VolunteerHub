6827e2dfdfe422efa9ccf36dcf54d725eca6edd1
dodat17 <clone14159265358@gmail.com>
2025-12-06 22:16:20 +0700

    feat(comment): add service, route and controller for comment feature

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import { authRouter } from "./presentation/routes/auth.routes";
 import { userRouter } from "./presentation/routes/user.route";
 import { eventRouter } from "./presentation/routes/event.route";
 import { postRouter } from "./presentation/routes/posts.routes";
+import { commentRouter } from "./presentation/routes/comment.routes"
 import cors from "cors";
 
 const app = express();
@@ -29,6 +30,7 @@ app.use("/api/events", eventRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/comments", commentRouter);
 
 // Global error handler (must be last)
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
