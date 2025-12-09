aea9fbc235e9bc3b18e4788df8bde219714dcc05
chicongn <chicongvn1205@gmail.com>
2025-12-09 19:36:53 +0700

    feat(router): add authenticate and authorize middleware

--- backend/src/presentation/routes/stats.routes.ts
+++ backend/src/presentation/routes/stats.routes.ts
@@ -4,12 +4,46 @@ import { statsController } from "../controllers/stats.controller";
 import { validate } from "../../middlewares/validation.middleware";
 import { EventStatsFilterSchema } from "../validators/stats/event-stats.schema";
 import { GetEventSchema } from "../validators/event/get-event.schema";
+import { authorize } from "../../middlewares/authorize.middleware";
+import { StatsPolicy } from "../../application/policies/stats.policy";
 
 export const statsRouter = Router();
 
-statsRouter.get("/", statsController.getOverviewStats);
-statsRouter.get("/events", validate(EventStatsFilterSchema), statsController.getEventsStats);
-statsRouter.get("/events/:eventId", validate(GetEventSchema), statsController.getEventStats);
-statsRouter.get("/trending", statsController.getTrendingEvents);
-statsRouter.get("/volunteers", statsController.getVolunteerStats);
-statsRouter.get("/managers", statsController.getEventManagersStats);
+statsRouter.get(
+    "/",
+    authenticate,
+    authorize(StatsPolicy.overview),
+    statsController.getOverviewStats
+);
+statsRouter.get(
+    "/events",
+    authenticate,
+    validate(EventStatsFilterSchema),
+    authorize(StatsPolicy.allEventsStats),
+    statsController.getEventsStats
+);
+statsRouter.get(
+    "/events/:eventId",
+    authenticate,
+    validate(GetEventSchema),
+    authorize(StatsPolicy.eventStats),
+    statsController.getEventStats
+);
+statsRouter.get(
+    "/trending",
+    authenticate,
+    authorize(StatsPolicy.trendingEvents),
+    statsController.getTrendingEvents
+);
+statsRouter.get(
+    "/volunteers",
+    authenticate,
+    authorize(StatsPolicy.volunteerStats),
+    statsController.getVolunteerStats
+);
+statsRouter.get(
+    "/managers",
+    authenticate,
+    authorize(StatsPolicy.eventManagersStats),
+    statsController.getEventManagersStats
+);
