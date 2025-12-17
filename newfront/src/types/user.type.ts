bd3f27221908c8300f2e31067ff73dca5dcb32ea
chicongn <chicongvn1205@gmail.com>
2025-12-17 13:03:28 +0700

    feat(type): define user interface

---
+++ newfront/src/types/user.type.ts
@@ -0,0 +1,32 @@
+import type { UserRole, UserStatus } from "./enum";
+
+export interface UserProfile {
+    id: string;
+    username: string;
+    email?: string;
+    avatarUrl: string;
+
+    role: UserRole;
+    status: UserStatus;
+
+    lastLogin?: Date | null;
+    updatedAt?: Date;
+}
+
+export interface User {
+    id: string;
+    username: string;
+    email?: string;
+    avatarUrl: string;
+
+    role: UserRole;
+    status: UserStatus;
+
+    notifications: string[];
+    postIds: string[];
+    participatedEventIds: string[];
+    registeredEventIds: string[];
+
+    lastLogin?: Date | null;
+    updatedAt?: Date;
+}
